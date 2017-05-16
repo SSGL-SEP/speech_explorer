dd63ac53f65216e16e366c31395342912285e586
Aivanus <ivank1995@hotmail.com>
2017-05-16 16:50:00 +0300

    trying things out

--- app/visualizer.js
+++ app/visualizer.js
@@ -1,40 +1,60 @@
 var THREE = require("three");
 var BoilerPlate = require("./Boilerplate");
 
-var Visualizer = module.exports = function(x) {
+var Visualizer = module.exports = function (x) {
     var scope = this;
     BoilerPlate.call(this);
     this.name = "Visualizer";
     this.renderer = null;
 
-    this.init = function() {
+    this.init = function () {
         this.createEnvironment();
+        window.setInterval(this.tst, 100);
     };
-    
-    this.createEnvironment = function() {
+
+    this.createEnvironment = function () {
         this.renderer = new THREE.WebGLRenderer({
-            antialias : true
+            antialias: true
         });
-        this.renderer.setClearColor( 0x0F0F0F );
-        this.renderer.setSize( window.innerWidth, window.innerHeight );
+        this.renderer.setClearColor(0x0F0F0F);
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
         this.renderer.setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1);
         this.context = document.getElementById('visualizer');
-        this.context.appendChild(this.renderer.domElement); 
-        this.scene = new THREE.Scene();
-        this.camera = new THREE.OrthographicCamera( 
-            window.innerWidth / - 2, 
-            window.innerWidth / 2, 
-            window.innerHeight / 2, 
-            window.innerHeight / - 2, 
-            -2000, 2000 );
+        this.context.appendChild(this.renderer.domElement);
 
-        this.camera.position.x = 0;
-        this.camera.position.y = 0;
-        this.camera.position.z = 100;
+        this.scene = new THREE.Scene();
+
+        this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
         this.scene.add(this.camera);
 
-        this.base = new THREE.Object3D();
-        this.scene.add(this.base);
+        // this.camera = new THREE.OrthographicCamera(
+        //     window.innerWidth / - 2,
+        //     window.innerWidth / 2,
+        //     window.innerHeight / 2,
+        //     window.innerHeight / - 2,
+        //     -2000, 2000);
+
+        // this.camera.position.x = 0;
+        // this.camera.position.y = 0;
+        this.camera.position.z = 20;
+        // this.scene.add(this.camera);
+
+        // this.base = new THREE.Object3D();
+        // this.scene.add(this.base);
+
+        var geometry = new THREE.BoxGeometry(1, 1, 1);
+        var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        var cube = new THREE.Mesh(geometry, material);
+        this.scene.add(cube);
+
+        this.renderer.render(this.scene, this.camera)
+
+
+        // this.render = function () {
+        //     requestAnimationFrame(this.render);
+        //     this.renderer.render(this.scene, this.camera);
+        // };
+
     };
 };
 Visualizer.prototype = new BoilerPlate();

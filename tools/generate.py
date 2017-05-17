194c8695f3a05d37f410c6ea9f54fe611d7c6f2d
Karvonen <antti.karvonen@gmail.com>
2017-05-17 10:07:15 +0300

    fake data generator

--- tools/generate.py
+++ tools/generate.py
@@ -5,15 +5,17 @@ import json
 import random
 
 #{
-#    "1": [100, 200, 300, "url", ["male age50"]"],
-#    "2": [200, 100, 300, "url", ["female age25"]]
+#    "1": [100, 200, 300, "url", ["tag1 tag2"]"],
+#    "2": [200, 100, 300, "url", ["tag1 tag2"]]
 #}
 
 
 
 def main(argv):
     if len(argv) != 3:
-        sys.exit("Usage: ./generate.py Number OutputFile")
+        print("Generates json like this:")
+        print("\"1\": [100, 200, 300, \"url\", [\"tag1 tag2\"]\"]")
+        sys.exit("Usage: ./generate.py Elements OutputFile")
     data = generate(argv[1])
     write_file(data, argv[2])
 
@@ -22,15 +24,16 @@ def generate(elements):
     data = []
     genders = ['male', 'female']
     for i in range(1, int(elements) + 1):
-        x = random.randint(50, 2000)
-        y = random.randint(50, 2000)
-        z = random.randint(50, 2000)
-        age_tag = random.randint(1,110)
+        x = random.randint(50, 800)
+        y = random.randint(50, 800)
+        z = random.randint(50, 800)
+        age_tag = random.randint(1,111)
         gender_tag = genders[random.randint(0,len(genders)-1)] 
         item = {str(i): [x, y, z, "url", [gender_tag + " " + str(age_tag)]]}
         data.append(item)
 
     return data
+
 
 def write_file(data, filename):
     with open(filename, 'w') as outfile:

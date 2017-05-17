48a1e2536f84239720022464d96c998c2367d2d9
yussiv <jussi.voipio@cs.helsinki.fi>
2017-05-17 09:52:37 +0300

    webpack jiggering

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.get('/', function(request, response) {
     response.sendFile(__dirname + '/index.html');
 });
 
+app.use(express.static('public'));
+
 app.listen(port, function() {
     console.log("Server listening in port " + port);
 });

498818cf7a1ae5319456b673dacadf1b2b3a4f30
Sopulius <aleksi.suoranta@gmail.com>
2017-05-18 13:43:42 +0300

    mocha configured + istanbul for coverage

---
+++ test/visualizerTest.js
@@ -0,0 +1,33 @@
+var assert = require('assert');
+var THREE = require("three");
+var BoilerPlate = require("../app/Boilerplate");
+var Visualizer = require('../app/visualizer');
+var vis = new Visualizer();
+
+describe('hooks', function() {
+
+  before(function() {
+    // runs before all tests in this block
+    
+  });
+
+  after(function() {
+    // runs after all tests in this block
+  });
+
+  beforeEach(function() {
+    // runs before each test in this block
+  });
+
+  afterEach(function() {
+    // runs after each test in this block
+  });
+
+  // test cases
+  describe('Visualizer#name', function(){
+    it('should be Visualizer', function(){
+      assert(vis.name === "Visualizer");
+    });
+  })
+  
+});

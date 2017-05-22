3e403f943a9b48727e445b1027fe216992361935
yussiv <jussi.voipio@cs.helsinki.fi>
2017-05-22 13:50:17 +0300

    decoupled Data.js from actual data

--- app/Data.js
+++ app/Data.js
@@ -1,26 +1,26 @@
 var THREE = require("three");
-var jsonData = require("../realdata.json");
 
-var stringified = JSON.stringify(jsonData);
-var jsonParsed = JSON.parse(stringified);
-var parsedData = [], total = jsonParsed.length;
+var parsedData = [], total = 0;
 var parsedUrls = []
-
-
-for (var i = 0; i < total; i++) {
-    parsedData.push(new THREE.Vector3(jsonParsed[i][1], jsonParsed[i][2], jsonParsed[i][3]));
-    parsedUrls.push(jsonParsed[i][4]);
-}
 
 var Data = module.exports = {
     pointSize: 1,
     cloudSize2D: 1.5,
 
+    loadData: function(data) {
+        parsedData = [];
+        total = data.length;
+        parsedUrls = [];
+        for (var i = 0; i < data.length; i++) {
+            parsedData.push(new THREE.Vector3(data[i][1], data[i][2], data[i][3]));
+            parsedUrls.push(data[i][4]);
+        }
+    },
 
     getTotalPoints: function () {
         return total;
     },
-    
+
     getUrl: function(index){
         return parsedUrls[index];
     },
--- entry.js
+++ entry.js
@@ -1,3 +1,7 @@
+var Data = require('./app/Data');
+var json = require('./realdata.json');
+Data.loadData(json);
+
 var Visualizer = require("./app/Visualizer");
 var visualizer = new Visualizer();
 visualizer.init();

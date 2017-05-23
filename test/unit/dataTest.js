a56c33c831593dad3e431c034be6bf83693eb373
Aivanus <ivank1995@hotmail.com>
2017-05-23 14:52:45 +0300

    made first test for testing JSON tag parsing

--- test/unit/dataTest.js
+++ test/unit/dataTest.js
@@ -3,46 +3,57 @@ var assert = require('assert');
 var THREE = require("three");
 var Data;
 
-describe('hooks', function() {
+describe('hooks', function () {
 
-	before(function() {
+	before(function () {
 		Data = require(appDir + "/app/Data");
 		var json = require(appDir + "/test/testdata.json");
 		Data.loadData(json);
-  	});
-
-	after(function() {
-		
 	});
 
-	beforeEach(function() {
-		
+	after(function () {
+
 	});
 
-	afterEach(function() {
-		
+	beforeEach(function () {
+
+	});
+
+	afterEach(function () {
+
 	});
 
 
-	describe('Data#total', function(){
-		it('should be 10', function(){
-		  	assert(Data.getTotalPoints() === 10);
+	describe('Data#total', function () {
+		it('should be 10', function () {
+			assert(Data.getTotalPoints() === 10);
 		});
 	})
 
-	describe('Data#parsedUrls(0)', function(){
-		it('should be audio/mv_0763_019_a_1_1.wav', function(){
-		  	assert(Data.getUrl(0) === "audio/mv_0763_019_a_1_1.wav");
+	describe('Data#parsedUrls(0)', function () {
+		it('should be audio/mv_0763_019_a_1_1.wav', function () {
+			assert(Data.getUrl(0) === "audio/mv_0763_019_a_1_1.wav");
 		});
 	})
 
-	describe('Data#getPosition(0)', function(){
-		it('should be x === 95.6850419438296, y === 244.51765847203887, z === 469.97548434392615', function(){
+	describe('Data#getPosition(0)', function () {
+		it('should be x === 95.6850419438296, y === 244.51765847203887, z === 469.97548434392615', function () {
 			var test = Data.getPosition(0);
-		  	assert(test.isVector3);
+			assert(test.isVector3);
 			assert(test.x === 95.6850419438296);
 			assert(test.y === 244.51765847203887);
 			assert(test.z === 469.97548434392615);
+		});
+	})
+
+	describe('Data#parsedTags(1))', function () {
+		it('should be key === phonem, values === [i,h,e,n,l,a]', function () {
+			var testTag = Data.parsedTags(1),
+				testValues = [i, h, e, n, l, a];
+			assert(testTag.key === 'phonem');
+			for (var i = 0; i < 10; i++) {
+				assert(testTag.values[i] === testValues[i]);
+			}
 		});
 	})
 

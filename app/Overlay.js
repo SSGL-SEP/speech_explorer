76f63e4809ad2997f8f11d6a03be77d5f12b4442
Sopulius <aleksi.suoranta@gmail.com>
2017-05-23 14:40:50 +0300

    prototype

--- app/Overlay.js
+++ app/Overlay.js
@@ -2,22 +2,38 @@ var dat = require("dat.gui/build/dat.gui.min.js");
 
 
 var gui = new dat.GUI({ autoPlace: false });
-var filter = {
-    a: false,
-    e: false,
-    voiced: false,
-    unvoiced: false
+
+var testPhonem = {
+    key: "phonem",
+    values: ["a", "e", "i", "o", "u"]
 };
-var obj = { Filter:function(){ console.log("clicked") }};
+
+var testVoiced = {
+    key: "voiced",
+    values: ["unvoiced", "voiced"]
+};
+
+var testGender = {
+    key: "gender",
+    values: ["male","female"]
+}
+
+var test = [testPhonem, testVoiced, testGender];
+
+var obj = { Filter: function () { console.log("clicked") } };
 
 
+for (var i = 0; i < test.length; i++) {
+    var tag = test[i];
+    var phonem = gui.addFolder(tag.key);
+    var o = {}
+    for (var j = 0; j < tag.values.length; j++) {
+        o[tag.values[j]] = true;
+        phonem.add(o, tag.values[j]);
+    }
+}
 
-var phonem = gui.addFolder("Phonem");
-phonem.add(filter,"a");
-phonem.add(filter,"e");
-var voiced = gui.addFolder("Voiced");
-voiced.add(filter,"voiced");
-gui.add(obj,'Filter');
+gui.add(obj, 'Filter');
 var doc = document.getElementById('overlay');
 doc.appendChild(gui.domElement);
 

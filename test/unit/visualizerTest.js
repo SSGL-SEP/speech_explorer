7531279ed94e3a31513f4b5f0d193370eda896e8
lrkr <lkarvonen@gmail.com>
2017-05-23 10:37:54 +0300

    data tests

--- test/unit/visualizerTest.js
+++ test/unit/visualizerTest.js
@@ -7,28 +7,28 @@ var vis = new Visualizer();
 
 describe('hooks', function() {
 
-  before(function() {
-    // runs before all tests in this block
+	before(function() {
+		// runs before all tests in this block
 
-  });
+	});
 
-  after(function() {
-    // runs after all tests in this block
-  });
+	after(function() {
+	    // runs after all tests in this block
+	});
 
-  beforeEach(function() {
-    // runs before each test in this block
-  });
+	beforeEach(function() {
+	// runs before each test in this block
+	});
 
-  afterEach(function() {
-    // runs after each test in this block
-  });
+	afterEach(function() {
+    	// runs after each test in this block
+  	});
 
-  // test cases
-  describe('Visualizer#name', function(){
-    it('should be Visualizer', function(){
-      assert(vis.name === "Visualizer");
-    });
-  })
+  	// test cases
+  	describe('Visualizer#name', function(){
+    	it('should be Visualizer', function(){
+      		assert(vis.name === "Visualizer");
+    	});
+  	})
 
 });

ab86e07ba4031c1371d56a185db1b146154d9d73
Sopulius <aleksi.suoranta@gmail.com>
2017-05-23 22:04:45 +0300

    cleaning

--- app/Overlay.js
+++ app/Overlay.js
@@ -6,30 +6,13 @@ var dat = require("dat.gui/build/dat.gui.min.js");
 
 
 var Overlay = module.exports = function (tags) {
-    var testPhonem = {
-        key: "phonem",
-        values: ["a", "e", "i", "o", "u"]
-    };
-
-    var testVoiced = {
-        key: "voiced",
-        values: ["unvoiced", "voiced"]
-    };
-
-    var testGender = {
-        key: "gender",
-        values: ["male", "female"]
-    }
-    console.log(tags);
-    var test = tags;
-
     var gui = new dat.GUI({ autoPlace: false });
 
     var obj = { Filter: function () { console.log("clicked") } };
 
 
-    for (var i = 0; i < test.length; i++) {
-        var tag = test[i];
+    for (var i = 0; i < tags.length; i++) {
+        var tag = tags[i];
         var phonem = gui.addFolder(tag.key);
         var o = {}
         for (var j = 0; j < tag.values.length; j++) {

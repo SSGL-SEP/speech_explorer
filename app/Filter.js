13f3c26b45024196f8c47611f54e83ac5859d755
yussiv <jussi.voipio@cs.helsinki.fi>
2017-05-24 13:33:23 +0300

    filter skeleton

--- app/Filter.js
+++ app/Filter.js
@@ -1,76 +1,35 @@
 
 var Data = require("./Data");
+var PointCloud = require("./PointCloud");
+var _ = require("underscore");
+
+var activePoints = [];
 
 
-var State = module.exports = {
-    FILTER_VISIBLE: 2,
-    FILTER_HIDDEN: 0.075,
-    FILTER_LARGE: 4,
-    
-    filterStates: [],
-    filterWords: [],
-    filteredResult: [],
-
-
-    getFilterStates: function() {
-        return this.filterStates;
-    },
-
-    getFilterState: function(index) {
-        return this.filterStates[index];
-    },
-
-    resetFilterStates: function() {
-        var total = Data.getTotalPoints();
-        var i = 0;
-        for (; i < total; i++) {
-            this.filterStates[i] = this.FILTER_VISIBLE;
-        }
-    },
-
-    setFilterState: function(index, newState) {
-        this.filterState[index] = newState;
-    },
-
-    getFilterWords: function() {
-        return this.filterWords;
-    },
+var Filter = module.exports = {
 
     resetFilter: function() {
-        var i = 0;
-        var total = Data.getTotalPoints();
-        for(; i < total; i++) {
-            this.filterStates[i] = this.FILTER_VISIBLE;
-        }
-        this.filterWords = [];
+        activePoints = [];
     },
 
-    setFilter: function(list) {
-        var total = Data.getTotalPoints();
-        this.filterWords = list;
-
-        for (var i = 0; i < total; i++) {
-            var tags = Data.getTags();
-            var tagLength = tags.length;
-
-            for (var j = 0; j < tagsLength; j++) {
-                //if clause here to compare tags to object in list parameter
-            
-            }
-        }
+    getActivePoints: function() {
+        return activePoints;
     },
 
-    getFilteredList: function() {
-        var list = [];
-        var total = Data.getTotalPoints();
-        for (var i = 0; i < total; i++) {
-            if (filterStates[i] === FILTER_LARGE) {
-                list.push(i);
-            }
-        }
-        return list;
+    setFilter: function(activeTags) {
+        var activeLists = [];
+        activeTags.forEach(function(activeTag) {
+            var tag = Data.getTag(activeTag.key);
+            var values = tag.values;
+            values.forEach(function(value) {
+                activeTag.values.forEach(function(activeTag) {
+                    if(activeTag === value.value) {
+                        activeLists.push(value.points);
+                    }
+                });
+            });
+        });
+        activePoints = _.intersection(activeLists);
     }
 
-}
-
-
+};

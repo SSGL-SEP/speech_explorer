ba24335a43ee40e70fc306d190c1df3e09382afd
Ivan Kropotov <ivank1995@hotmail.com>
2017-05-25 15:11:12 +0300

    started refactoring of the Data.js

--- app/Data.js
+++ app/Data.js
@@ -14,20 +14,16 @@ var Data = module.exports = {
 
     loadData: function (data) {
         parsedData = [];
-        parsedUrls = [];
         parsedTags = [];
         var hues = [];
-        var metaData = {};
         total = data.length;
 
         var i;
         for (i = 0; i < data.length; i++) {
             var dataPoint = new THREE.Vector3(data[i][1], data[i][2], data[i][3]);
             dataPoint.url = "audio/" + data[i][4];
-            dataPoint.meta = this.parseMetadata(data[i][5]);
+            dataPoint.meta = this.parseTags(data[i][5], i);;
             parsedData.push(dataPoint);
-
-            this.parseTags(data[i][5], i);
 
             var x = Math.pow(parsedData[i].x, 2);
             var y = Math.pow(parsedData[i].y, 2);
@@ -46,42 +42,126 @@ var Data = module.exports = {
             parsedData[i].color = color;
 
         }
+        console.log(parsedTags);
+        for (var index = 0; index < 10; index++) {
+            console.log(parsedData[index]);;
+
+        }
     },
 
     // Parses tag JSON into tag objects
+    // parseTags: function (tags, pointIndex) {
+    //     for (var i = 0; i < tags.length; i++) {
+    //         this.tag = tags[i];
+
+    //         if (this.getTagIndex(parsedTags, this.tag.key) === -1) {
+    //             parsedTags.push({ key: this.tag.key, values: [] });
+    //         }
+
+    //         this.tagIndex = this.getTagIndex(parsedTags, this.tag.key);
+    //         this.values = parsedTags[this.tagIndex].values;
+    //         if (this.getValueIndex(this.values, this.tag.val) === -1) {
+    //             this.values.push({ value: this.tag.val, points: [] });
+    //         }
+
+    //         this.valueIndex = this.getValueIndex(this.values, this.tag.val);
+    //         this.values[this.valueIndex].points.push(pointIndex);
+    //     }
+    // },
+
     parseTags: function (tags, pointIndex) {
+        var meta = [];
         for (var i = 0; i < tags.length; i++) {
-            this.tag = tags[i];
+            var tagKey = tags[i].key;
+            var tagVal = tags[i].val;
 
-            if (this.getTagIndex(parsedTags, this.tag.key) === -1) {
-                parsedTags.push({ key: this.tag.key, values: [] });
-            }
+            // Parse tag for a point object 
+            var tagIndex = this.addTagObject(meta, tagKey);
+            meta[tagIndex].values.push(tagVal);
 
-            this.tagIndex = this.getTagIndex(parsedTags, this.tag.key);
-            this.values = parsedTags[this.tagIndex].values;
-            if (this.getValueIndex(this.values, this.tag.val) === -1) {
-                this.values.push({ value: this.tag.val, points: [] });
-            }
-
-            this.valueIndex = this.getValueIndex(this.values, this.tag.val);
-            this.values[this.valueIndex].points.push(pointIndex);
+            // Parse tag for parsedTag array
+            tagIndex = this.addTagObject(parsedTags, tagKey);
+            var values = parsedTags[tagIndex].values;
+            var valueIndex = this.addValueObject(values, tagVal);
+            values[valueIndex].points.push(pointIndex);
         }
+        // Returns array of tag objects for use as a property
+        // of a point object
+        return meta;
     },
 
-    parseMetadata: function (tags) {
-        var meta = [];
+    // parseMetadata: function (tags) {
+    //     var meta = [];
 
-        for (var i = 0; i < tags.length; i++) {
-            this.tag = tags[i];
+    //     for (var i = 0; i < tags.length; i++) {
+    //         var tag = tags[i];
 
-            if (this.getTagIndex(meta, this.tag.key) === -1) {
-                meta.push({ key: this.tag.key, values: [] });
-            }
+    //         var tagKey = tag.key;
+    //         var tagVal = tag.val;
 
-            this.tagIndex = this.getTagIndex(meta, this.tag.key);
-            this.values = meta[this.tagIndex].values.push(this.tag.val);
+
+    //         var tagIndex = this.addTagObject(meta, tagKey);
+    //         meta[tagIndex].values.push(tagVal);
+    //     }
+    //     return meta;
+    // },
+    // parseMetadata: function (tags) {
+    //     var meta = [];
+
+    //     for (var i = 0; i < tags.length; i++) {
+    //         var tag = tags[i];
+
+    //         var tagKey = tag.key;
+    //         var tagVal = tag.val;
+
+
+    //         var tagIndex = this.addTagObject(meta, tagKey);
+    //         meta[tagIndex].values.push(tagVal);
+    //     }
+    //     return meta;
+    // },
+
+    // parseMetadata: function (tags) {
+    //     var meta = [];
+
+    //     for (var i = 0; i < tags.length; i++) {
+    //         this.tag = tags[i];
+
+    //         if (this.getTagIndex(meta, this.tag.key) === -1) {
+    //             meta.push({ key: this.tag.key, values: [] });
+    //         }
+
+    //         this.tagIndex = this.getTagIndex(meta, this.tag.key);
+    //         this.values = meta[this.tagIndex].values.push(this.tag.val);
+    //     }
+    //     return meta;
+    // },
+
+
+    // Adds tag object to an array if it doesn't exist. 
+    // Returns index of tag object.
+    addTagObject: function (array, tagKey) {
+        var tagIndex = this.getTagIndex(array, tagKey);
+        // Checks if tag key already exists
+        if (tagIndex === -1) {
+            // if no, creates new tag object and initializes value array
+            array.push({ key: tagKey, values: [] });
+            return array.length - 1;
         }
-        return meta;
+        return tagIndex;
+    },
+
+    // Adds a value object to an array if it doesn't exist. 
+    // Returns index of a value object.
+    addValueObject: function (array, tagVal) {
+        var valueIndex = this.getValueIndex(array, tagVal);
+        // Checks if value is present in values array
+        if (valueIndex === -1) {
+            // if no, creates new value object and initializes array of point object indexes
+            array.push({ value: tagVal, points: [] });
+            return array.length - 1;
+        }
+        return valueIndex;
     },
 
     getTagIndex: function (array, key) {
@@ -93,9 +173,9 @@ var Data = module.exports = {
         return -1;
     },
 
-    getValueIndex: function (values, value) {
-        for (var i = 0; i < values.length; i++) {
-            if (values[i].value === value) {
+    getValueIndex: function (array, value) {
+        for (var i = 0; i < array.length; i++) {
+            if (array[i].value === value) {
                 return i;
             }
         }
@@ -118,8 +198,6 @@ var Data = module.exports = {
         return parsedData[index].url;
     },
 
-    // TODO: Rename this function as it returns more than
-    // position information
     getPosition: function (index) {
         return parsedData[index];
     },

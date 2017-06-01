8cc91f6fb780d013cb6a5d7c271fe912b5bb5441
lrkr <lkarvonen@gmail.com>
2017-06-01 14:04:14 +0300

    selenium taas

--- test/features/step_definitions/filter_steps.js
+++ test/features/step_definitions/filter_steps.js
@@ -4,15 +4,17 @@ const {defineSupportCode} = require('cucumber');
 
 defineSupportCode(function({Given, When, Then}) {
     Then('I should see all points being active', function() {
-        return this.driver.findElement(By.css("#active")).getText().then(function(element) {
-            return expect(element).to.equal('200/200 active');
+        return this.driver.findElement(By.css("#active")).then(function(element) {
+            element.getText().then(function(element) {
+                return expect(element).to.equal('200/200 active');
+            });
         });
     });
 
     Then('I should see {stringInDoubleQuotes} active samples', function(activeAmount) {
         return this.driver.findElement(By.css("#active")).then(function(element) {
-                element.getText().then(function(element) {
-                    return expect(element).to.equal(activeAmount + '/200 active');
+            element.getText().then(function(element) {
+                return expect(element).to.equal(activeAmount + '/200 active');
             });
         });
     });

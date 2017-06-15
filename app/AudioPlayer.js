a4665ee2bdb4a37952f130020f8281c4137cc935
Aivanus <ivank1995@hotmail.com>
2017-06-15 13:36:34 +0300

    added back option to play sound from path instead of blob

--- app/AudioPlayer.js
+++ app/AudioPlayer.js
@@ -1,59 +1,80 @@
 'use strict';
 
+var Data = require('./Data');
 var sounds = [];
 var current = null;
 var playingEnabled = true;
+var audioFile = null;
 
 var playSound = function(index) {
-    if (current) {
-        current.stop(0);
-        current = null;
-    }
-    var context = sounds[index].context;
-    var source = context.createBufferSource();
-    source.buffer = sounds[index].buffer;
-    source.connect(context.destination);
-    current = source;
-    current.onended = clearCurrent;
-    source.start(0);
-    return source;
+	console.log('playing from blob');
+	if (current) {
+		current.stop(0);
+		current = null;
+	}
+	var context = sounds[index].context;
+	var source = context.createBufferSource();
+	source.buffer = sounds[index].buffer;
+	source.connect(context.destination);
+	current = source;
+	current.onended = clearCurrent;
+	source.start(0);
+	return source;
+};
+
+var playSoundFromPath = function(path) {
+	console.log('playing from path');
+	if (audioFile !== null) {
+		audioFile.pause();
+		audioFile.startTime = 0;
+	}
+	audioFile = new Audio(path);
+	audioFile.play().catch(function() {
+		//whatever
+	});
 };
 
 var clearCurrent = function() {
-    current = null;
+	current = null;
 };
 
 var iterateSounds = function(soundIndexes, index) {
-    if (index >= soundIndexes.length) {
-        return;
-    }
-    if (playingEnabled) {
-        var source = playSound(soundIndexes[index]);
-        source.addEventListener('ended', function() {
-            iterateSounds(soundIndexes, index + 1);
-        });
+	if (index >= soundIndexes.length) {
+		return;
+	}
+	if (playingEnabled) {
+		var source = playSound(soundIndexes[index]);
+		source.addEventListener('ended', function() {
+			iterateSounds(soundIndexes, index + 1);
+		});
 
-    } else {
-        playingEnabled = true;
-    }
+	} else {
+		playingEnabled = true;
+	}
 };
 
 var AudioPlayer = module.exports = {
 
-    loadSounds: function(array) {
-        sounds = array;
-    },
+	loadSounds: function(array) {
+		sounds = array;
+	},
 
-    playSound: playSound,
+	playSound: function(index) {
+		if (sounds) {
+			playSound(index);
+		} else {
+			playSoundFromPath(Data.getUrl(index));
+		}
+	},
 
     /**
      * @param {number[]} soundIndexes
      */
-    playSounds: function(soundIndexes) {
-        iterateSounds(soundIndexes, 0);
-    },
+	playSounds: function(soundIndexes) {
+		iterateSounds(soundIndexes, 0);
+	},
 
-    stop: function() {
-        playingEnabled = false;
-    }
+	stop: function() {
+		playingEnabled = false;
+	}
 };

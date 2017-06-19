9d7fa58cdbf13bbbfc70a5238e7dee1e47a49dd5
lrkr <lkarvonen@gmail.com>
2017-06-19 10:15:19 +0300

    selection circle instead of ring

--- app/SelectionCursor.js
+++ app/SelectionCursor.js
@@ -2,52 +2,48 @@
 
 var THREE = require("three");
 
-var mouseMesh;
+var circle;
 var size;
-var borderWidth = 2;
+var radius;
 
 
-var changeSize = function() {
-    // mouseMesh.geometry.parameters.outerRadius = size;
-    // mouseMesh.geometry.parameters.innerRadius = size - borderWidth;
-
-    // console.log(mouseMesh.geometry);
-
-    mouseMesh.scale.set(size, size, 0);
-
-};
 
 module.exports = {
 
     init: function(initialSize) {
         size = initialSize;
-        var mouseGeometry = new THREE.RingGeometry(size - borderWidth, initialSize, 64);
-        var mouseMaterial = new THREE.MeshBasicMaterial({
+        var mouseGeometry = new THREE.CircleGeometry(size, 128);
+        var mouseMaterial = new THREE.LineBasicMaterial({
             color: 0xFF0000
         });
-        mouseMesh = new THREE.Mesh(mouseGeometry, mouseMaterial);
-        mouseMesh.visible = false;
+        mouseGeometry.vertices.shift();
+        circle = new THREE.Line(mouseGeometry, mouseMaterial);
+        circle.autoClose = true;
+        circle.position.set(10000,10000);
+        circle.visible = false;
     },
 
     update: function(event) {
-        mouseMesh.position.set(event.clientX + window.innerWidth / -2, -event.clientY - window.innerHeight / -2, 10);
+        circle.position.set(event.clientX + window.innerWidth / -2, -event.clientY - window.innerHeight / -2, 10);
     },
 
     changeMode: function(mode) {
         if (mode === 0) {
-            mouseMesh.visible = false;
+            circle.visible = false;
         } else if (mode === 1) {
-            mouseMesh.material.color.setHex(0x00FF00);
+            circle.material.color.setHex(0x00FF00);
         } else if (mode === 2) {
-            mouseMesh.material.color.setHex(0xFF0000);
+            circle.material.color.setHex(0xFF0000);
         }
         if (mode !== 0) {
-            mouseMesh.visible = true;
+            circle.visible = true;
         }
     },
 
     setScale: function(scale) {
-        mouseMesh.scale.set(scale, scale, 1);
+        circle.scale.set(scale, scale, 1);
+        circle.geometry.parameters.innerRadius = radius * scale;
+        console.log(circle);
     },
 
     getSize: function() {
@@ -55,7 +51,7 @@ module.exports = {
     },
 
     getMesh: function() {
-        return mouseMesh;
+        return circle;
     }
 };
 

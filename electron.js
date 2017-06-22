a847c9cda2e47f135ecc93702f462e8e1330fa0e
Sopulius <aleksi.suoranta@gmail.com>
2017-06-22 11:22:45 +0300

    devtools off

--- electron.js
+++ electron.js
@@ -38,7 +38,7 @@ app.on('ready', function () {
   mainWindow.maximize();
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {

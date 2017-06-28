3919dc6080119e98db3448b923d3e7d9b878658f
Sopulius <aleksi.suoranta@gmail.com>
2017-06-28 15:42:35 +0300

    fixed issues

--- electron.js
+++ electron.js
@@ -12,43 +12,43 @@ const url = require('url')
 var mainWindow = null;
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd   Q
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
+app.on('window-all-closed', function() {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd   Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-app.on('ready', function () {
-  // Create the browser window and disable integration with node
-  mainWindow = new BrowserWindow({
-    nodeIntegration: false
-  });
+app.on('ready', function() {
+    // Create the browser window and disable integration with node
+    mainWindow = new BrowserWindow({
+        nodeIntegration: false
+    });
 
-  //mainWindow.webContents.openDevTools();
+    //mainWindow.webContents.openDevTools();
 
-  mainWindow.maximize();
+    mainWindow.maximize();
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'public/index.html'),
-    protocol: 'file:',
-    slashes: true
-  }));
+    // and load the index.html of the app.
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'public/index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
 
-  
 
-  // Open the DevTools.
-  
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+    // Open the DevTools.
+
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
 });
